8ab8fc315ce6efa7a2e16e079e4002abc62a7a9b
JellyCZ <62903038+JellyCZYT@users.noreply.github.com>
2022-05-26 17:08:37 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+        printf("Tipni si cislo od 1 do 1000\n");
+        int number;
+        scanf("%d", &number);
+        if (number == 954) {
+        printf("Vyhral jsi!\n");
+        }
+        else {
+        printf("Prohral jsi!\n");
+}
+        return 0;
+}
